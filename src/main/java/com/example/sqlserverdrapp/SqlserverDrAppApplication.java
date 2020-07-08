dbe326da99e9d4618b807c98589f1ed0463da2e1
Dylan Roberts <dyroberts@pivotal.io>
2020-07-08 14:52:04 -0400

    Initial commit

---
+++ src/main/java/com/example/sqlserverdrapp/SqlserverDrAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlserverdrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlserverDrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlserverDrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sqlserverdrapp/SqlserverDrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlserverdrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlserverDrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
